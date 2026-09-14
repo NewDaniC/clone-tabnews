5b4fe1a6b8abe9aaa7c5036a03da55cd5e0bda18
NewDaniC <danieltheisgesdacosta@gmail.com>
2026-09-14 14:37:03 -0400

    makes `database.js` `client.connect()` more robust to errors

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -10,10 +10,8 @@ test("GET to /api/v1/status should return 200", async () => {
 
   /* Server Version */
   expect(responseBody.dependencies.database.version).toEqual("16.0");
-
   /* Max Connections */
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
   /* Opened Connections */
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
